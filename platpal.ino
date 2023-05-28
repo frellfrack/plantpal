9162bdedbcea44eae0746895c902288abf4b2b6f
frellfrack <89649483+frellfrack@users.noreply.github.com>
2023-05-28 15:34:55 +0100

    Update platpal.ino
    Imroved Dashboard

--- platpal.ino
+++ platpal.ino
@@ -16,11 +16,15 @@ float temp =0;
 float hum =0;
 int moisture=0;
 
-const int AirValue = 2800;   //you need to replace this value with Value_1
-const int WaterValue = 1100;  //you need to replace this value with Value_2
+const int AirValue = 2650;   //you need to replace this value with Value_1
+const int WaterValue = 1210;  //you need to replace this value with Value_2
 
-const char* ssid = "europa";
-const char* password = "VvahTUMdwv5RJDQW";
+//const char* ssid = "europa";
+//const char* password = "VvahTUMdwv5RJDQW";
+
+const char* ssid = "SKYTQBQV";
+const char* password = "EmpyQQyAawtw";
+
 WebServer server(80);
 const int led = 13;
 
@@ -40,22 +44,33 @@ void handleReadings() {
 
 }
 
+
+
+void randomFact() {
+  String ptr ="\n{\"randomfact\":\"";
+  ptr +=getRandomPlantFact();
+  ptr +="\"}\n";
+  
+  server.send(200, "application/json", ptr);
+
+}
+
+
 String returnHeader()
 {
   String ptr = "<!DOCTYPE html>\n<html lang=\"enGB\">\n";
   ptr += "<head>\n";
     ptr += "<meta charset=\"UTF-8\">\n";
   ptr += "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr += "<title>Plant Pal V1</title>\n";
-
- 
+  ptr += "<title>Plant Pal V0.2</title>\n";
   ptr += "<script src=\"https://code.jquery.com/jquery-3.7.0.min.js\" integrity=\"sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=\" crossorigin=\"anonymous\"></script>\n"; // Add jQuery library
   ptr += "<script src=\"https://cdn.jsdelivr.net/npm/chart.js\"></script>\n"; // Add Chart.js library
+  
+  ptr += "<link rel=\"stylesheet\" href=\"/style.css\">\n";
   ptr += "</head>\n";
   ptr += "<body>\n";
-  ptr += "<header><h1>Plant Pal V0.1</h1></header>\n";
+  ptr += "<header><h1>Plant Pal V0.2</h1></header>\n";
  
-  ptr += "<link rel=\"stylesheet\" href=\"/style.css\">\n";
 
   ptr += "<nav>\n";
   
@@ -63,7 +78,7 @@ String returnHeader()
     
   ptr += "<a href=\"/settings/\">Settings</a>\n";
   ptr += "</nav>\n";
-   ptr += "<p>\n";
+   ptr += "<p id=\"randomFact\">\n";
 ptr+=getRandomPlantFact();
    ptr += "</p>\n";
   ptr += "<main>\n";
@@ -195,6 +210,7 @@ void displaySettings()
   digitalWrite(led, 0);
 }
 
+
 void handleJS() {
   digitalWrite(led, 1);
   String ptr = "var chartData = [];\n"; // Array to store the historical data
@@ -208,7 +224,14 @@ void handleJS() {
   ptr += "    $(\"#hum\").html(data.hum+\"%\");\n";
   ptr += "    $(\"#moist\").html(data.moisture+\"%\");\n";
 
+  // Add code to retain only the last 8 readings
+  ptr += "    if (chartData.length >= 8) {\n";
+  ptr += "      chartData.shift();\n"; // Remove the oldest reading
+  ptr += "    }\n";
+
+  ptr += "    var currentTime = new Date().toLocaleTimeString();\n"; // Get the current timestamp
   ptr += "    chartData.push({\n"; // Push the new data to the array
+  ptr += "      time: currentTime,\n"; // Store the current timestamp
   ptr += "      temp: data.temp,\n";
   ptr += "      hum: data.hum,\n";
   ptr += "      moisture: data.moisture\n";
@@ -222,42 +245,34 @@ void handleJS() {
   ptr += "    myChart.destroy();\n"; // Destroy the existing chart instance if it exists
   ptr += "  }\n";
 
-  ptr += "  var labels = chartData.map(function(entry) {\n"; // Extract labels from the array
-  ptr += "    return '';\n"; // You can customize the labels here
-  ptr += "  });\n";
-  ptr += "  var tempData = chartData.map(function(entry) {\n"; // Extract temperature data from the array
-  ptr += "    return entry.temp;\n";
-  ptr += "  });\n";
-  ptr += "  var humData = chartData.map(function(entry) {\n"; // Extract humidity data from the array
-  ptr += "    return entry.hum;\n";
-  ptr += "  });\n";
-  ptr += "  var moistData = chartData.map(function(entry) {\n"; // Extract moisture data from the array
-  ptr += "    return entry.moisture;\n";
+  // Extract time data from the array
+  ptr += "  var timeData = chartData.map(function(entry) {\n";
+  ptr += "    return entry.time;\n";
   ptr += "  });\n";
 
   ptr += "  var ctx = document.getElementById('chart').getContext('2d');\n"; // Get the chart canvas context
   ptr += "  myChart = new Chart(ctx, {\n";
   ptr += "    type: 'line',\n";
   ptr += "    data: {\n";
-  ptr += "      labels: labels,\n"; // Use the extracted labels
+  ptr += "      labels: timeData,\n"; // Use the extracted time data for labels
   ptr += "      datasets: [\n";
   ptr += "        {\n";
   ptr += "          label: 'Temperature',\n";
-  ptr += "          data: tempData,\n"; // Use the extracted temperature data
+  ptr += "          data: chartData.map(function(entry) { return entry.temp; }),\n"; // Use the extracted temperature data
   ptr += "          borderColor: 'rgba(255, 99, 132, 1)',\n";
   ptr += "          backgroundColor: 'rgba(255, 99, 132, 0.2)',\n";
   ptr += "          borderWidth: 1\n";
   ptr += "        },\n";
   ptr += "        {\n";
   ptr += "          label: 'Humidity',\n";
-  ptr += "          data: humData,\n"; // Use the extracted humidity data
+  ptr += "          data: chartData.map(function(entry) { return entry.hum; }),\n"; // Use the extracted humidity data
   ptr += "          borderColor: 'rgba(54, 162, 235, 1)',\n";
   ptr += "          backgroundColor: 'rgba(54, 162, 235, 0.2)',\n";
   ptr += "          borderWidth: 1\n";
   ptr += "        },\n";
   ptr += "        {\n";
   ptr += "          label: 'Moisture',\n";
-  ptr += "          data: moistData,\n"; // Use the extracted moisture data
+  ptr += "          data: chartData.map(function(entry) { return entry.moisture; }),\n"; // Use the extracted moisture data
   ptr += "          borderColor: 'rgba(75, 192, 192, 1)',\n";
   ptr += "          backgroundColor: 'rgba(75, 192, 192, 0.2)',\n";
   ptr += "          borderWidth: 1\n";
@@ -265,7 +280,7 @@ void handleJS() {
   ptr += "      ]\n";
   ptr += "    },\n";
   ptr += "    options: {\n";
-    ptr += "      animation: {\n";
+  ptr += "      animation: {\n";
   ptr += "        duration: 0 // Set animation duration to 0 to disable animation\n";
   ptr += "      },\n";
 
@@ -278,11 +293,26 @@ void handleJS() {
   ptr += "  });\n";
   ptr += "}\n";
 
-  ptr += "setInterval(timer, 1500);\n";
+  ptr += "setInterval(timer, 3000);\n";
+
+  ptr += "function getRandomFact()\n";
+  ptr += "{\n";
+  ptr += "$.getJSON(\"/randomfact.json\", function(data) {\n";
+
+
+  ptr += "console.log(data);\n";
+  ptr += "$(\"#randomFact\").text(data.randomfact);\n";
+    
+  ptr += "});\n";
+   
+  ptr += "}\n";
+
+  ptr += "setInterval(getRandomFact, 10000);\n";
 
   server.send(200, "text/javascript", ptr);
   digitalWrite(led, 0);
 }
+
 
 void handleNotFound() {
   digitalWrite(led, 1);
@@ -577,6 +607,7 @@ void setup() {
 
   server.on("/", handleRoot);
   server.on("/readings.json", handleReadings);
+  server.on("/randomfact.json", randomFact);
   server.on("/style.css",handleCSS);
   server.on("/settings/",displaySettings);
   server.on("/app.js",handleJS);
@@ -609,7 +640,15 @@ void printValues()
 
     int soilMoistureValue = analogRead(moistureSensorPin);
     moisture  = map(soilMoistureValue, AirValue, WaterValue, 0, 100);
-
+    if(moisture > 100)
+    {
+      moisture =100;
+    }
+    if(moisture<0)
+    {
+      moisture =0;
+    }
+    
     sprintf(str, "%s,%s,%i", dtostrf(temp,4,2,buff),dtostrf(hum,4,2,buff2),moisture);
     Serial.println(str);
     u8g2.clearBuffer(); // clear the internal memory
